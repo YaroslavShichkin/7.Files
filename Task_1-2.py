96fbc8ef20868fee267fb2cec17d50bb8434b7f8
Yaroslav Shichkin <yaroslavshichkin@mail.ru>
2022-09-29 19:14:07 +0300

    Task_2

--- Task_1-2.py
+++ Task_1-2.py
@@ -10,4 +10,17 @@ with open('Files/recipes.txt', 'r', encoding='utf8') as file:
         file.readline()
         cook_book.setdefault(line.strip(), food)
 
-print(cook_book)
+# print(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    x = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            if ingredient['ingredient_name'] in x.keys():
+                pass
+                x[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity'])*person_count
+            else:
+                x.setdefault(ingredient['ingredient_name'], {'measure': ingredient['measure'], 'quantity': int(ingredient['quantity'])*person_count})
+    return(x)
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
